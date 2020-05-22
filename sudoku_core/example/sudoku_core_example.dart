e02701afb28752f0070e8ab8e55ded38dab62049
KalilDev <kalildev@users.noreply.github.com>
2020-05-22 18:28:47 -0300

    all: remove broken examples and tests

--- sudoku_core/example/sudoku_core_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:sudoku_core/sudoku_core.dart';
-
-void main() {
-  //var awesome = Awesome();
-  //print('awesome: ${awesome.isAwesome}');
-}
--- sudoku_presentation/example/sudoku_presentation_example.dart
+++
@@ -1,6 +0,0 @@
-import 'package:sudoku_presentation/sudoku_presentation.dart';
-
-void main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
-}
