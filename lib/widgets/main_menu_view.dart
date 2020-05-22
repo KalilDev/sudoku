3b957b7b831220882d24b003879f813d802bb32e
KalilDev <kalildev@users.noreply.github.com>
2020-05-22 15:47:36 -0300

    main_menu: Update for new bloc features
    * Show errors
    * Only show continue if supported
    * Notify the user once if his platform isn't supported

--- lib/widgets/main_menu_view.dart
+++ lib/widgets/main_menu_view.dart
@@ -1,13 +1,12 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:provider/provider.dart';
+import 'package:sudoku/theme.dart';
+import 'package:sudoku/widgets/board/board.dart';
 import 'package:sudoku_presentation/common.dart';
 import 'package:sudoku_presentation/main_menu_bloc.dart';
+
 import 'prefs_sheet.dart';
-import 'package:sudoku/theme.dart';
-import 'package:provider/provider.dart';
-import 'package:sudoku/widgets/board/board.dart';
 import 'sudoku_button.dart';
 
 String difficultyToString(SudokuDifficulty difficulty) {
@@ -31,9 +30,25 @@ class MainMenu extends StatelessWidget {
     BlocProvider.of<MainMenuBloc>(context).add(ChangeX(difficulty));
   }
 
-  void launch(SudokuConfiguration config, BuildContext context) {
+  Future<void> launch(SudokuConfiguration config, StorageAknowledgment aknowledgment, BuildContext context) async {
+    final needsResponse = aknowledgment == StorageAknowledgment.unsupported;
+    if (needsResponse) {
+      final didAccept = await showDialog<bool>(context: context, builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text("Aviso"),
+          content: Text("Você está usando uma plataforma que ainda não suporta salvar Sudokus. Caso você saia, todo o seu progresso será perdido."),
+          actions: [
+            FlatButton(onPressed: ()=>Navigator.of(context).pop(true), child: Text("Aceitar"))
+          ],
+        );
+      });
+      if (!didAccept) {
+        return;
+      }
+      BlocProvider.of<MainMenuBloc>(context).add(AknowledgeStorageEvent());
+    }
     final difficultyIndex = SudokuDifficulty.values.indexOf(config.difficulty);
-    Navigator.of(context).pushNamed("${config.side}x$difficultyIndex", arguments: config).then((_) => BlocProvider.of<MainMenuBloc>(context).add(ReloadConfigurations()));
+    return Navigator.of(context).pushNamed("${config.side}x$difficultyIndex", arguments: config).then((_) => BlocProvider.of<MainMenuBloc>(context).add(ReloadConfigurations()));
   }
 
   @override
@@ -41,14 +56,20 @@ class MainMenu extends StatelessWidget {
     return BlocBuilder<MainMenuBloc, MainMenuState>(
         builder: (BuildContext context, MainMenuState _state) {
       if (_state is LoadingMainMenu) {
-        return Center(child: CircularProgressIndicator());
+        return const Center(child: CircularProgressIndicator());
+      }
+      if (_state is MainMenuErrorState) {
+        return Scaffold(body: Center(child: Column(mainAxisSize: MainAxisSize.min,children: [
+          Text(_state.userFriendlyMessage),
+          Text("Mensagem do erro: ${_state.message}")
+        ],),),);
       }
       final state = _state as MainMenuSnap;
       final configs = state.configurations;
       final theme = Provider.of<SudokuTheme>(context);
       final sideCounts = configs.map((row) => row.first.side).toList();
       final config = state.configurations[state.sideY][state.difficultyX];
-      final widthConstraints = BoxConstraints(maxWidth: 450);
+      const widthConstraints = BoxConstraints(maxWidth: 450);
       final boardWidget = 
                 Flexible(
                   child: Center(
@@ -81,17 +102,18 @@ class MainMenu extends StatelessWidget {
                   max: configs.width - 1.0,
                 ),
                 SudokuButton(
-                  onPressed: () => launch(config.newSudoku(), context),
+                  onPressed: () => launch(config.newSudoku(), state.storage, context),
                   child: Text("Novo jogo"),
                   filled: true,
                 ),
-                SudokuButton(
-                  onPressed: config.source == StateSource.storage
-                      ? () => launch(config, context)
-                      : null,
-                  filled: true,
-                  child: Text("Continuar"),
-                )
+                if (state.storage == StorageAknowledgment.supported)
+                  SudokuButton(
+                    onPressed: config.source == StateSource.storage
+                        ? () => launch(config, state.storage, context)
+                        : null,
+                    filled: true,
+                    child: Text("Continuar"),
+                  )
       ];
       return SliderTheme(
         data: sliderTheme,
