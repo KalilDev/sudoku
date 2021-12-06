e9636f5bfbbf3195fe61a250f9161545d6365a05
KalilDev <kalilgamer0@gmail.com>
2021-12-06 16:11:30 -0300

    sudoku: main: Hookup the MD3 theme engine

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,49 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:sudoku_presentation/common.dart';
-import 'package:sudoku_presentation/preferences_bloc.dart';
-import 'package:sudoku_presentation/main_menu_bloc.dart';
-import 'package:sudoku_presentation/sudoku_bloc.dart';
-import 'package:sudoku/repositories/localstorage_board_repository.dart';
-import 'package:sudoku_presentation/repositories.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:material_widgets/material_widgets.dart';
+import 'package:provider/provider.dart';
 import 'package:sudoku/repositories/cross_preferences_repository.dart';
+import 'package:sudoku/repositories/localstorage_board_repository.dart';
 import 'package:sudoku/theme.dart';
 import 'package:sudoku/widgets/board/sudoku_board_view.dart';
 import 'package:sudoku/widgets/main_menu_view.dart';
-import 'package:provider/provider.dart';
+import 'package:sudoku_presentation/exception_handler_bloc.dart';
+import 'package:sudoku_presentation/main_menu_bloc.dart';
+import 'package:sudoku_presentation/models.dart';
+import 'package:sudoku_presentation/preferences_bloc.dart';
+import 'package:sudoku_presentation/repositories.dart';
+import 'package:sudoku_presentation/sudoku_bloc.dart';
+
 
 void main() {
-  runApp(MultiRepositoryProvider(
-      providers: [
-        RepositoryProvider<BoardRepository>(
-            create: (_) => LocalStorageBoardRepository()),
-        RepositoryProvider<PreferencesRepository>(
-            create: (_) => CrossPreferencesRepository()),
-      ],
-      child: BlocProvider<PreferencesBloc>(
-        create: (BuildContext context) => PreferencesBloc(
-            RepositoryProvider.of<PreferencesRepository>(context)),
-        child: RootView(),
-      )));
+  runPlatformThemedApp(
+    BlocProvider<ExceptionHandlerBloc>(
+      create: (_) => ExceptionHandlerBloc(),
+      child: MultiRepositoryProvider(
+        providers: [
+          RepositoryProvider<BoardRepository>(
+              create: (_) => LocalStorageBoardRepository()),
+          RepositoryProvider<PreferencesRepository>(
+              create: (_) => CrossPreferencesRepository()),
+        ],
+        child: BlocProvider<PreferencesBloc>(
+          create: (BuildContext context) => PreferencesBloc(
+              preferencesRepository:
+                  RepositoryProvider.of<PreferencesRepository>(context),
+              onException:
+                  BlocProvider.of<ExceptionHandlerBloc>(context).handler),
+          child: RootView(),
+        ),
+      ),
+    ),
+    initialOrFallback: () => PlatformPalette.fallback(
+      primaryColor: Color(0xFF0d6da5),
+    ),
+  );
 }
 
 class RootView extends StatelessWidget {
@@ -55,65 +73,87 @@ class RootView extends StatelessWidget {
             ));
   }
 
+  static MonetTheme monetThemeFromSudokuTheme(SudokuTheme theme) =>
+      generateTheme(
+        theme.main,
+        tertiarySeed: theme.secondary,
+      ).override(
+        // Only one will be used, so its ok to change both brightness
+        light: (t) => t.copyWith(
+          background: theme.background,
+          surface: theme.background,
+        ),
+        dark: (t) => t.copyWith(
+          background: theme.background,
+          surface: theme.background,
+        ),
+      );
+
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<PreferencesBloc, PrefsState>(
       builder: (BuildContext context, PrefsState _state) {
         final state = _state is PrefsSnap ? _state : null;
-
-        final availableTheme = (state?.theme) ?? AvailableTheme.materialLight;
+        final availableTheme = state?.theme ?? AvailableTheme.monetAuto;
         final theme = SudokuTheme.availableThemeMap[availableTheme];
-        final colorScheme = theme.brightness == Brightness.light
-            ? ColorScheme.light(
-                primary: theme.main,
-                secondary: theme.secondary,
-                primaryVariant: theme.mainDarkened,
-                secondaryVariant: theme.secondaryDarkened)
-            : ColorScheme.dark(
-                primary: theme.main,
-                secondary: theme.secondary,
-                primaryVariant: theme.mainDarkened,
-                secondaryVariant: theme.secondaryDarkened);
-        SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
-        final isDark = theme.brightness == Brightness.dark;
-        final overlayStyle = SystemUiOverlayStyle(
-            statusBarBrightness: isDark ? Brightness.dark : Brightness.light,
-            statusBarIconBrightness:
-                isDark ? Brightness.light : Brightness.dark,
-            statusBarColor: Colors.transparent,
-            systemNavigationBarDividerColor: Colors.transparent,
-            systemNavigationBarColor: theme.background,
-            systemNavigationBarIconBrightness:
-                isDark ? Brightness.light : Brightness.dark);
-        final sliderTheme = SliderThemeData(
-            activeTrackColor: theme.secondary, thumbColor: theme.secondary);
-        final dialogTheme = DialogTheme(
-            backgroundColor:
-                Color.alphaBlend(theme.main.withAlpha(10), theme.background),
-            shape: RoundedRectangleBorder(
-                side: BorderSide(color: theme.mainDarkened, width: 2.0),
-                borderRadius: BorderRadius.circular(8.0)));
-        final themeData = ThemeData.from(
-                colorScheme: colorScheme.copyWith(
-                    background: theme.background, surface: theme.background))
-            .copyWith(sliderTheme: sliderTheme, dialogTheme: dialogTheme);
-        return AnnotatedRegion<SystemUiOverlayStyle>(
-          value: overlayStyle,
-          child: Provider<SudokuTheme>.value(
-            value: theme,
-            child: MaterialApp(
-              theme: themeData.copyWith(
-                  buttonTheme: themeData.buttonTheme.copyWith(
-                      buttonColor: theme.secondary,
-                      textTheme: ButtonTextTheme.primary)),
-              title: "Sudoku",
-              home: BlocProvider<MainMenuBloc>(
-                  create: (BuildContext context) => MainMenuBloc(
-                      RepositoryProvider.of<BoardRepository>(context),
-                      RepositoryProvider.of<PreferencesRepository>(context)),
-                  child: MainMenu()),
-              onGenerateRoute: onGeneratedRoute,
+
+        final themeMode = availableTheme == AvailableTheme.monetAuto
+            ? ThemeMode.system
+            : availableTheme == AvailableTheme.monetDark ||
+                    theme?.brightness == Brightness.dark
+                ? ThemeMode.dark
+                : ThemeMode.light;
+
+        if (_state is PrefsErrorState) {
+          return MaterialApp(
+            home: Scaffold(
+              appBar: MD3CenterAlignedAppBar(title: const Text('Erro')),
+              body: Center(
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(_state.error.getText(kDebugMode)),
+                    Text('Mensagem do erro: ' +
+                        _state.error.getExtraText(kDebugMode))
+                  ],
+                ),
+              ),
             ),
+          );
+        }
+
+        final isMonet = availableTheme == AvailableTheme.monetAuto ||
+            availableTheme == AvailableTheme.monetLight ||
+            availableTheme == AvailableTheme.monetDark;
+
+        return MD3Themes(
+          monetThemeForFallbackPalette:
+              isMonet ? null : monetThemeFromSudokuTheme(theme),
+          usePlatformPalette: isMonet,
+          builder: (context, light, dark) => MaterialApp(
+            theme: light,
+            darkTheme: dark,
+            themeMode: themeMode,
+            title: "Sudoku",
+            builder: (context, home) => AnimatedMonetColorScheme(
+              themeMode: themeMode,
+              child: Provider<SudokuTheme>.value(
+                value: sudokuThemeFromMonetScheme(context.colorScheme),
+                child: home,
+              ),
+            ),
+            home: BlocProvider<MainMenuBloc>(
+              create: (BuildContext context) => MainMenuBloc(
+                boardRepository:
+                    RepositoryProvider.of<BoardRepository>(context),
+                preferencesRepository:
+                    RepositoryProvider.of<PreferencesRepository>(context),
+                onException:
+                    BlocProvider.of<ExceptionHandlerBloc>(context).handler,
+              ),
+              child: MainMenu(),
+            ),
+            onGenerateRoute: onGeneratedRoute,
           ),
         );
       },
