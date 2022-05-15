5acc74367bece3c52c1bb24bc117eb3f457d554c
KalilDev <kalilgamer0@gmail.com>
2022-05-15 12:26:14 -0300

    main: use HomeView

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -2,11 +2,14 @@ import 'dart:typed_data';
 
 import 'package:app/base/controller.dart';
 import 'package:app/base/sudoku_data.dart';
+import 'package:app/home_view/controller.dart';
+import 'package:app/home_view/view.dart';
 import 'package:app/view/controller.dart';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:material_widgets/material_widgets.dart';
 import 'package:path_provider/path_provider.dart' as pp;
+import 'package:value_notifier/value_notifier.dart';
 
 import 'base/hive.dart';
 import 'ui/view.dart';
@@ -97,23 +100,22 @@ class MyApp extends StatelessWidget {
   }
 }
 
+final homeController = ControllerBase.create(() => HomeViewController());
+
 class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
+    return MD3AdaptativeScaffold(
+      appBar: const MD3CenterAlignedAppBar(
         title: Text("Sudoku"),
       ),
-      body: SudokuView(
-        controller: controller.handle,
+      body: MD3ScaffoldBody.noMargin(
+        child: HomeView(
+          controller: homeController.handle,
+        ),
       ),
-      /*floatingActionButton: FloatingActionButton(
-        onPressed: () => print("bar"),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),*/
     );
   }
 }
