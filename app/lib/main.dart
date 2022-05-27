fe60beea2e267d6bc6f3ef88fae0f20923fe2b1d
KalilDev <kalilgamer0@gmail.com>
2022-05-27 00:02:49 -0300

    main: remove bs

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -27,64 +27,12 @@ void main() async {
   Hive.registerAdapter(SudokuHomeItemAdapter());
   Hive.registerAdapter(SudokuDifficultyAdapter());
   Hive.init(await pp.getTemporaryDirectory().then((d) => d.path));
-  final fooBox = await Hive.openBox('foo');
-  final bdr = SudokuAppBoardStateBuilder(9);
-  bdr.solvedBoard = [
-    Uint8List.fromList([8,4,2,7,1,3,6,9,5]),
-    Uint8List.fromList([3,6,1,9,5,4,8,2,7]),
-    Uint8List.fromList([5,7,9,6,8,2,4,3,1]),
-    Uint8List.fromList([2,1,6,3,7,5,9,4,8]),
-    Uint8List.fromList([3,5,8,2,9,6,7,1,3]),
-    Uint8List.fromList([7,9,3,1,4,8,5,6,2]),
-    Uint8List.fromList([9,8,5,4,2,1,3,7,6]),
-    Uint8List.fromList([1,3,4,8,6,7,2,5,9]),
-    Uint8List.fromList([6,2,7,5,3,9,1,8,4]),
-  ];
-  bdr.fixedNumbers = [
-    Uint8List.fromList([0,0,0,7,1,3,6,9,5]),
-    Uint8List.fromList([0,0,0,9,5,4,8,2,7]),
-    Uint8List.fromList([0,0,0,6,8,2,4,3,1]),
-    Uint8List.fromList([0,0,0,3,7,5,9,4,8]),
-    Uint8List.fromList([0,0,0,2,9,6,7,1,3]),
-    Uint8List.fromList([0,0,0,1,4,8,5,6,2]),
-    Uint8List.fromList([0,0,0,4,2,1,3,7,6]),
-    Uint8List.fromList([0,0,0,8,6,7,2,5,9]),
-    Uint8List.fromList([0,0,0,5,3,9,1,8,4]),
-  ];
-  bdr.currentNumbers = [
-    Uint8List.fromList([8,4,2,0,0,0,0,0,0]),
-    Uint8List.fromList([3,6,1,0,0,0,0,0,0]),
-    Uint8List.fromList([5,7,9,0,0,0,0,0,0]),
-    Uint8List.fromList([0,0,0,0,0,0,0,0,0]),
-    Uint8List.fromList([0,0,0,0,0,0,0,0,0]),
-    Uint8List.fromList([0,0,0,0,0,0,0,0,0]),
-    Uint8List.fromList([0,0,0,0,0,0,0,0,0]),
-    Uint8List.fromList([0,0,0,0,0,0,0,0,0]),
-    Uint8List.fromList([0,0,0,0,0,0,0,0,0]),
-  ];
-  bdr.currentPossibilities = [
-    [[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[]],
-    [[1],[2],[3],[],[],[],[],[],[]],
-    [[],[],[],[],[],[],[],[],[]],
-    [[1,2],[1,2,3],[1,2,3,4],[],[],[],[],[],[]],
-  ];
-  final boardState = bdr.build();
-  final m = tileMatrixFromState(boardState);
-  controller = SudokuViewController(SudokuController.fromInitialState(fooBox, boardState), 9);
-  controller.init();
-  
+
   runPlatformThemedApp(
     const MyApp(),
     initialOrFallback: () => PlatformPalette.fallback(primaryColor: Color(0xDEADBEEF)),
   );
 }
-
-late final SudokuViewController controller;
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
