9734f7c1e2e98d34430a2ad5f58fedd8112e3669
KalilDev <kalilgamer0@gmail.com>
2022-06-01 14:11:30 -0300

    sudoku_generation: improve resource cleanup on generateSudokuStreamingIsolateLocal

--- app/lib/sudoku_generation/impl/generate_isolate_local.dart
+++ app/lib/sudoku_generation/impl/generate_isolate_local.dart
@@ -1,4 +1,5 @@
 // An streaming sudoku generation function that can block the current isolate.
+import 'dart:async';
 import 'dart:math';
 
 import 'package:app/module/base.dart';
@@ -36,52 +37,61 @@ Future<SolvedAndChallengeBoard> generateSudokuIsolateLocal(
 Stream<SudokuGenerationEvent> rawGenerateSudokuStreamingIsolateLocal(
   int side,
   int targetFilledCellCount,
-) async* {
-  final rand = Random();
-  final targetEmptyCellCount = side * side - targetFilledCellCount;
+) {
+  final streamController = StreamController<SudokuGenerationEvent>();
+
+  // Only two boards that will be allocated
   final solvedBoard = generateExternSudokuBlocking(side);
-
-  yield SudokuGenerationFoundSolution(sudokuBoardFromExtern(solvedBoard));
-
-  // Indices which we will try to remove
-  final indices = List<SudokuBoardIndex>.generate(
-    side * side,
-    (i) => SudokuBoardIndex(i % side, i ~/ side),
-    growable: false,
-  )..shuffle(rand);
   final workingBoard = cloneExternSudokuBoard(solvedBoard);
-  int emptyCellCount = 0;
-  for (final index in indices) {
-    // If we already reached the empty cell target, the remaining values are the
-    // solved values, which are already in the working board. Therefore, we can
-    // stop trying to zero out other cells
-    if (emptyCellCount >= targetEmptyCellCount) {
-      break;
+
+  streamController.addStream(() async* {
+    yield SudokuGenerationFoundSolution(sudokuBoardFromExtern(solvedBoard));
+    final rand = Random();
+    final targetEmptyCellCount = side * side - targetFilledCellCount;
+    // Indices which we will try to remove
+    final indices = List<SudokuBoardIndex>.generate(
+      side * side,
+      (i) => SudokuBoardIndex(i % side, i ~/ side),
+      growable: false,
+    )..shuffle(rand);
+    int emptyCellCount = 0;
+    for (final index in indices) {
+      // If we already reached the empty cell target, the remaining values are the
+      // solved values, which are already in the working board. Therefore, we can
+      // stop trying to zero out other cells
+      if (emptyCellCount >= targetEmptyCellCount) {
+        break;
+      }
+      // try zeroing out this index on the working board and checking if it still
+      // has only one sol
+      final solvedValue = externSudokuBoardGetAt(solvedBoard, index);
+      // zero out
+      externSudokuBoardSetAt(workingBoard, index, 0);
+      // check if there still is only one sol
+      final hasOnlyOneSolWhenZero = externSudokuHasOneSolBlocking(workingBoard);
+      if (hasOnlyOneSolWhenZero) {
+        emptyCellCount++;
+      } else {
+        // reset the value at index to be the solved value, in order for the
+        // sudoku to have only one sol
+        externSudokuBoardSetAt(workingBoard, index, solvedValue);
+        // yield the event signaling that the value at index is the solved value
+        yield SudokuGenerationFoundSquare(index, solvedValue);
+      }
     }
-    // try zeroing out this index on the working board and checking if it still
-    // has only one sol
-    final solvedValue = externSudokuBoardGetAt(solvedBoard, index);
-    // zero out
-    externSudokuBoardSetAt(workingBoard, index, 0);
-    // check if there still is only one sol
-    final hasOnlyOneSolWhenZero = externSudokuHasOneSolBlocking(workingBoard);
-    if (hasOnlyOneSolWhenZero) {
-      emptyCellCount++;
-    } else {
-      // reset the value at index to be the solved value, in order for the
-      // sudoku to have only one sol
-      externSudokuBoardSetAt(workingBoard, index, solvedValue);
-      // yield the event signaling that the value at index is the solved value
-      yield SudokuGenerationFoundSquare(index, solvedValue);
-    }
-  }
-  final result = SudokuGenerationFinished(
-    sudokuBoardFromExtern(solvedBoard),
-    sudokuBoardFromExtern(workingBoard),
-  );
-  externSudokuFree(solvedBoard);
-  externSudokuFree(workingBoard);
-  // We either could not zero out any more cells, or we reached the zeroed out
-  // cell target.
-  yield result;
+    // We either could not zero out any more cells, or we reached the zeroed out
+    // cell target.
+    yield SudokuGenerationFinished(
+      sudokuBoardFromExtern(solvedBoard),
+      sudokuBoardFromExtern(workingBoard),
+    );
+  }());
+
+  // Ensure we cleanup the 2 boards we just allocated
+  streamController.done.then((_) {
+    externSudokuFree(solvedBoard);
+    externSudokuFree(workingBoard);
+  });
+
+  return streamController.stream;
 }
