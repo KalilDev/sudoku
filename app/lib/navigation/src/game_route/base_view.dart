e63cae8855cf1e5d9db85cb991de3d2ecf895c26
KalilDev <kalilgamer0@gmail.com>
2022-06-02 18:00:34 -0300

    app: navigation: base_view: remove unneeded imports

--- app/lib/navigation/src/game_route/base_view.dart
+++ app/lib/navigation/src/game_route/base_view.dart
@@ -1,16 +1,8 @@
-import 'package:app/module/base.dart';
-import 'package:app/navigation/src/game_route/data.dart';
-import 'package:app/sudoku_generation/sudoku_generation.dart';
 import 'package:app/util/l10n.dart';
-import 'package:app/view/home.dart';
 import 'package:app/view/preferences_dialog.dart';
-import 'package:app/view/sudoku_generation.dart';
-import 'package:app/viewmodel/sudoku_board.dart';
-import 'package:app/viewmodel/sudoku_generation.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:material_widgets/material_widgets.dart';
-import 'package:value_notifier/value_notifier.dart';
 
 class BaseSudokuView extends StatelessWidget {
   const BaseSudokuView({
